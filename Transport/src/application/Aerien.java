ec062476482c2c05f68d7787699a2b6eee6129b5
Canan <bekir.canan@etu.univ-lyon1.fr>
2024-04-14 16:39:47 +0200

    charger_graphe

--- Transport/src/application/Aerien.java
+++ Transport/src/application/Aerien.java
@@ -1,5 +1,10 @@
 package application;
 
+import construction.Graphe;
+import java.io.IOException;
+import java.util.Scanner;
+import org.graphstream.graph.Graph;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -13,9 +18,14 @@ public class Aerien {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(System.in);
+        System.out.println("entrez l'emplacement des fichiers(sans .txt)");
+        String file = scan.nextLine();
+        Graph g = Graphe.chargerGraphe("DataTest\\"+file+".txt");
+        g.display();
     }
-    
 }
+
--- Transport/src/construction/Graphe.java
+++ Transport/src/construction/Graphe.java
@@ -4,10 +4,46 @@
  */
 package construction;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+
 /**
  *
  * @author bekir
  */
 public class Graphe {
-    
+     static int sommets;
+    public static Graph chargerGraphe(String nomFichier) throws FileNotFoundException, IOException{
+        Graph g = new SingleGraph("Chaine");
+        String arete1,arete2;
+        
+        g.setStrict(false);
+        g.setAutoCreate( true );
+        
+        try ( Scanner scan = new Scanner(new File("DataTest\\graph-test0.txt"))) {
+            g.addAttribute("kMax",scan.next());
+            sommets=scan.nextInt();
+            //scan.useDelimiter(" ");
+            while(scan.hasNext()){
+                arete1 = scan.next();
+                arete2= scan.next();
+                g.addEdge(arete1+"-"+arete2, arete1, arete2);
+            }
+                
+            for(Node n:g){
+                n.setAttribute("ui.style", "size:20px;");
+                n.setAttribute("ui.label", n.getId());
+            }
+            for(Edge e:g.getEdgeSet()){
+                e.setAttribute("ui.label", e.getId());
+            }
+        }
+        return g;
+    }
 }
